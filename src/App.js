1bf698d2c4f5bc00fbd7cd661014487f6ab1d867
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2024-08-04 22:19:21 -0600

    Carrusel de imagenes con error en el video

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer'
+import SliderComponent from './Components/Carousel';
 
 function App() {
     return (
         <div className="App">
             <Navbar/>
+            <SliderComponent/>
             <Footer/>
         </div>
     );
---
+++ src/Components/Carousel.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import './Carousel.css'; 
+
+// Importa tus imágenes y videos
+import image1 from './media/img-1.jpg';
+import image2 from './media/img-2.jpg';
+import video1 from './videos/vid1.mp4';
+import image3 from './media/img-3.jpg';
+
+const SliderComponent = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />
+  };
+
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src={image1} alt="Slide 1" />
+        </div>
+        <div>
+          <img src={image2} alt="Slide 2" />
+        </div>
+        <div>
+          <video controls width="100%" height="auto">
+            <source src={video1} type="video/mp4" />
+          </video>
+        </div>
+        <div>
+          <img src={image3} alt="Slide 3" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'black' }}
+      onClick={onClick}
+    />
+  );
+};
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: 'block', background: 'black' }}
+      onClick={onClick}
+    />
+  );
+};
+
+export default SliderComponent;
